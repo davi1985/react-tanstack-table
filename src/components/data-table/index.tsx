a5a9dbb4523da72bd78ea8d614984d022e6d0f98
Davi Silva <davisilvaphoto@gmail.com>
2025-09-15 16:25:18 -0300

    column size

--- src/components/data-table/index.tsx
+++ src/components/data-table/index.tsx
@@ -22,6 +22,9 @@ export const DataTable = <TData,>({ columns, data }: DataTableProps<TData>) => {
   const table = useReactTable({
     data,
     columns,
+    defaultColumn: {
+      size: 100,
+    },
     getCoreRowModel: getCoreRowModel(),
   });
 
@@ -31,7 +34,11 @@ export const DataTable = <TData,>({ columns, data }: DataTableProps<TData>) => {
         {table.getHeaderGroups().map((headerGroup) => (
           <TableRow key={headerGroup.id}>
             {headerGroup.headers.map((header) => (
-              <TableHead key={header.id}>
+              <TableHead
+                key={header.id}
+                colSpan={header.colSpan}
+                style={{ width: `${header.getSize()}px` }}
+              >
                 {!header.isPlaceholder &&
                   flexRender(
                     header.column.columnDef.header,
@@ -47,7 +54,10 @@ export const DataTable = <TData,>({ columns, data }: DataTableProps<TData>) => {
         {table.getRowModel().rows.map((row) => (
           <TableRow key={row.id}>
             {row.getAllCells().map((cell) => (
-              <TableCell key={cell.id}>
+              <TableCell
+                key={cell.id}
+                style={{ width: `${cell.column.getSize()}px` }}
+              >
                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
               </TableCell>
             ))}
